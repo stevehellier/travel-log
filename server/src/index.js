7c3e745f030d5c3364f0d651b9d81b27a087c958
Steve Hellier <steve_hellier@hotmail.com>
2020-07-03 08:49:01 +0100

    Added error handling and 404 errors

--- server/src/index.js
+++ server/src/index.js
@@ -1,10 +1,34 @@
 const express = require('express');
+const morgan = require('morgan');
+const helmet = require('helmet');
+const cors = require('cors');
+
+const middlewares = require('./middlewares');
+
+require('dotenv').config();
 
 const app = express();
+app.use(morgan('common'));
+app.use(helmet());
+app.use(
+   cors(
+      {origin: 'http://localhost:3000',
+   })
+);
 
-const server = process.env.SERVER || 'https://localhost';
+app.get('/', (req,res) => {
+   res.json({
+      message: 'hello, world!',
+   });
+});
+
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+
 const port = process.env.PORT || 1337;
 
-app.listen(server, port, () => {
-   console.log(`Listening at ${SERVER}:${PORT}`);
+app.listen(port, () => {
+   console.log(`Listening at 'http://localhost':${port}`);
 });
